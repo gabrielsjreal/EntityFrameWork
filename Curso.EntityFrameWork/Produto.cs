f56b41725b2b1c99c441104890fb774305d357d7
Gabriel Souza <gabrielsjreal>
2019-05-30 12:21:13 -0300

    Criação do método para add os dados no banco de dados

---
+++ Curso.EntityFrameWork/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Curso.EntityFrameWork
+{
+    public class Produto
+    {
+        public int Id { get;  set; }
+        public string Nome { get;  set; }
+        public string Categoria { get;  set; }
+        public double Preco { get;  set; }
+
+        public override string ToString()
+        {
+            return "Id: " + Id + "\n"+
+                    "Nome: " + Nome + "\n" +
+                    "Categoria: " + Categoria + "\n" +
+                    "Preço: " + Preco  ;
+        }
+    }
+}
---
+++ Curso.EntityFrameWork/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Curso.EntityFrameWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GravarUsandoEntity();
+            ExibirProdutos();
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Homem de Ferro";
+            p.Categoria = "Filmes";
+            p.Preco = 35.50;
+
+            var contexto = new LojaContext();
+            
+                contexto.Produtos.Add(p);
+                contexto.SaveChanges();
+            
+        }
+
+        private static void ExibirProdutos()
+        {
+            using (var banco = new LojaContext())
+            {
+                IList<Produto> produtos = banco.Produtos.ToList();
+                foreach (var item in produtos)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
